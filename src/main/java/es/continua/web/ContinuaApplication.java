50b6c83f631ffa1b2d93055c46cc2ff613713203
Jorge Torregrosa <jtorregrosalloret@gmail.com>
2016-03-10 21:07:55 +0100

    Created base project

---
+++ src/main/java/es/continua/web/ContinuaApplication.java
@@ -0,0 +1,14 @@
+package es.continua.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuaApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ContinuaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/es/continua/web/ContinuaServletContextListener.java
@@ -0,0 +1,24 @@
+package es.continua.web;
+
+        import javax.servlet.ServletContextEvent;
+        import javax.servlet.ServletContextListener;
+
+        import org.springframework.stereotype.Component;
+
+/**
+ * Simple {@link ServletContextListener} to test gh-2058.
+ */
+@Component
+public class ContinuaServletContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("*** contextInitialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("*** contextDestroyed");
+    }
+
+}
